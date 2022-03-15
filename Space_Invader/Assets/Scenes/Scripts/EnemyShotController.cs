14f99ce076a9c5b7bfa8e1c8bbc4fa03d79d09e8
Simon <sfierbeck@web.de>
2022-03-16 00:01:20 +0100

    first enemy

---
+++ Space_Invader/Assets/Scenes/Scripts/EnemyShotController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotController : MonoBehaviour
+{
+    public float horizontalSpeed = 3.0f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.y = position.y - horizontalSpeed * Time.deltaTime;
+        transform.position = position;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController >();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+
+}
--- Space_Invader/Assets/Scenes/Scripts/PlayerController.cs
+++ Space_Invader/Assets/Scenes/Scripts/PlayerController.cs
@@ -6,11 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     float horizontal; 
-    
+    public int maxHealth = 3;
+    int currentHealth;
+
+    public int health { get { return currentHealth; }}
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -26,5 +30,11 @@ public class PlayerController : MonoBehaviour
 
 
         rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
